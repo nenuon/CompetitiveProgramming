7ba61f94fb8df5798c0b04c0f4cc6f9a652a226d
Hagiwara Masaki <keep.my.best.61@gmail.com>
2019-03-21 20:32:59 +0900

    solved

---
+++ Atcoder/ABC118/B/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func main() {
+	sc := NewScanner(os.Stdin)
+	N := sc.NextInt()
+	M := sc.NextInt()
+	cnt := make([]int, M+1)
+	for i := 0; i < N; i++ {
+		K := sc.NextInt()
+		for j := 0; j < K; j++ {
+			a := sc.NextInt()
+			cnt[a]++
+		}
+	}
+	ans := 0
+	for i := 1; i <= M; i++ {
+		if cnt[i] == N {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// scanner
+type Scanner struct {
+	r   *bufio.Reader
+	buf []byte
+	p   int
+}
+
+func NewScanner(r io.Reader) *Scanner {
+	rdr := bufio.NewReaderSize(r, 1024)
+	return &Scanner{r: rdr}
+}
+
+func (s *Scanner) ReadLine() {
+	s.buf = make([]byte, 0)
+	for {
+		line, isp, err := s.r.ReadLine()
+		if err != nil {
+			os.Exit(1)
+		}
+		s.buf = append(s.buf, line...)
+		if !isp {
+			break
+		}
+	}
+}
+
+func (s *Scanner) NextInt() int {
+	i, err := strconv.Atoi(s.Next())
+	if err != nil {
+		os.Exit(1)
+	}
+	return i
+}
+
+func (s *Scanner) NextFloat64() float64 {
+	i, err := strconv.ParseFloat(s.Next(), 64)
+	if err != nil {
+		os.Exit(1)
+	}
+	return i
+}
+
+func (s *Scanner) Next() string {
+	s.Pre()
+	start := s.p
+	for ; s.p < len(s.buf); s.p++ {
+		if s.buf[s.p] == ' ' {
+			break
+		}
+	}
+	next := string(s.buf[start:s.p])
+	s.p++
+	return next
+}
+
+func (s *Scanner) Pre() {
+	if s.p >= len(s.buf) {
+		s.ReadLine()
+		s.p = 0
+	}
+}
